39a210f4e547b51493179f078b712415bae9822e
Jonathan Andrews <jonnyandrews.jma@gmail.com>
2018-12-16 12:23:44 +0000

    setting up linter(rubocop)

--- app.rb
+++ app.rb
@@ -1,7 +1,9 @@
+# frozen_string_literal: true
+
 require 'sinatra/base'
 
+# these are the routes for simple poker application
 class SimplePoker < Sinatra::Base
-
   get '/' do
     erb :configure_game
   end
--- spec/feature/configure_game_spec.rb
+++ spec/feature/configure_game_spec.rb
@@ -1,6 +1,8 @@
-feature "Configure Game screen" do
-  scenario "displayed when entering website" do
-    visit "/"
-    expect(page).to have_content("Hello")
+# frozen_string_literal: true
+
+feature 'Configure Game screen' do
+  scenario 'displayed when entering website' do
+    visit '/'
+    expect(page).to have_content("Welcome the World's Simplest Poker Game")
   end
 end
