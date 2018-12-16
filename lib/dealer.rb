97075f72766842dcbe0c926bff8515f5f43bdfca
Jonathan Andrews <jonnyandrews.jma@gmail.com>
2018-12-16 17:23:36 +0000

    allocating card to player deletes from card deck

--- lib/dealer.rb
+++ lib/dealer.rb
@@ -1,13 +1,14 @@
 class Dealer
-  CARDS = [1,2,3,4,5,6,7,8,9,10,
-           11,12,13,14,15,16,17,18,19,20,
-           21,22,23,24,25,26,27,28,29,30]
 
-  attr_reader :players, :hand_size
+  attr_reader :players, :hand_size, :cards
 
   def initialize(players, hand_size)
     @players = players
     @hand_size = hand_size
+    @cards = [1,2,3,4,5,6,7,8,9,10,11,12,13,
+              1,2,3,4,5,6,7,8,9,10,11,12,13,
+              1,2,3,4,5,6,7,8,9,10,11,12,13,
+              1,2,3,4,5,6,7,8,9,10,11,12,13]
   end
 
   def deal 
@@ -29,11 +30,19 @@ class Dealer
   def allocate_cards(player_hash)
     for i in 1..players do
       player = "player_" + i.to_s
-      hand = CARDS.sample(hand_size)
+      hand = select_cards(hand_size)
       player_hash[player.to_sym].push(*hand)
     end
-    p player_hash
     player_hash
+  end
+
+  def select_cards(quantity)
+    hand = []
+    for i in 1..quantity do
+      p cards
+      hand << cards.delete_at(rand(cards.length))
+    end
+    hand
   end
 
 end
